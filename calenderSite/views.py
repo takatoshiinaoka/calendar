b0372182a99b387db07553f6b86f1281dbf20ce5
ShunsukeHosino <hosino0625@gmail.com>
2021-09-01 15:48:03 +0900

    星野

--- calenderSite/views.py
+++ calenderSite/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html',{
 
-    })
+    }):
+
+#def serch(request):
+#    return render(request, 'serch.html')
