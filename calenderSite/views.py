4070456947ed599cc4e2dbbe006741eac9c1a88e
ShunsukeHosino <hosino0625@gmail.com>
2021-09-01 16:02:34 +0900

    星野 index用意

--- calenderSite/views.py
+++ calenderSite/views.py
@@ -4,5 +4,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-#def serch(request):
-#    return render(request, 'serch.html')
+"""def serch(request):
+    return render(request, 'serch.html')
+"""
+"""def addData(request):
+   return render(request, 'addData.html')
+"""
+"""def user(request):
+   return render(request, 'user.html')
+"""
+"""def subject(request):
+   return render(request, 'subject.html')
+"""
+
