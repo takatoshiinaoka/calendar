d5f0d308a02b0351478abd2686c939dc03cafda0
FIT IPC <fit-user@bene.fit.ac.jp>
2021-09-01 15:56:12 +0900

    西本　views

--- calenderSite/views.py
+++ calenderSite/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
-    return render(request, 'index.html',{
-
-    })
-=======
     return render(request, 'index.html')
->>>>>>> c5b24c103ca6c65620218ab0ebbfa6da844c016c
 
 #def serch(request):
 #    return render(request, 'serch.html')
