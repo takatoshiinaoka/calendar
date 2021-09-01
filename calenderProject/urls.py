ba2c19deeba200325afd315f40b307b71c15994b
FIT IPC <fit-user@bene.fit.ac.jp>
2021-09-01 15:17:12 +0900

    nisiimoto

--- calenderProject/urls.py
+++ calenderProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from calenderSite import views
 
 urlpatterns = [
-    path('', views.index,  name="index"),
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
