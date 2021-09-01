f7875581d7c77effeb923e69e7e562b5d3c3c88f
FIT IPC <fit-user@bene.fit.ac.jp>
2021-09-01 16:05:01 +0900

    DB作成

--- calenderSite/models.py
+++ calenderSite/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Kalender(models.Model):
+    subject = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    user = models.CharField(max_length=30)
