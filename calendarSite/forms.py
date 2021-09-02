81dba5bbc1ebe5972f9b858fe551ce8e34be1f17
ShunsukeHosino <hosino0625@gmail.com>
2021-09-02 10:00:07 +0900

    forms

---
+++ calendarSite/forms.py
@@ -0,0 +1,19 @@
+'''
+    form.py
+'''
+from django.core.files.storage import default_storage
+from django import forms
+
+class addDataForm(forms.Form):
+
+
+class indexForm(forms.Form):
+
+
+class searchForm(forms.Form):
+    
+
+class subjectForm(forms.Form):
+
+
+class userForm(forms.Form):
