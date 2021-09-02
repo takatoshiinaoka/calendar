cad0dcde2035dae96daa4d3151a7a7821afe57c9
ShunsukeHosino <hosino0625@gmail.com>
2021-09-02 12:05:19 +0900

    search subject登録

--- calendarSite/views.py
+++ calendarSite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from calendarSite.forms import addDataForm
 from calendarSite.forms import indexForm
-from calendarSite.forms import searchForm
+# from calendarSite.forms import searchForm
 from calendarSite.forms import subjectForm
 from calendarSite.forms import userForm
 
@@ -12,7 +12,30 @@ def index(request):
     return render(request, 'index.html')
 
 def search(request):
-    return render(request, 'search.html')
+   if request.POST:
+      # form = searchForm(request.POST or None)
+      subject = request.POST.getlist("subject") 
+      print(subject)
+   # メッセージ情報を取得。ない場合は空文字が入る
+   
+   """if  subject != '':
+        # メッセージの内容をDBに登録
+        CalendarModel = Calendar(subject=subject)
+        CalendarModel.save()
+
+    CalendarList = Calendar.objects.all()
+
+    return render(request, 'search.html',{
+        "CalendarList": CalendarList
+    })
+
+    dbData = {
+        "id": 123,
+        "name": "name",
+        "form": form,
+        "CalendarList" : CalendarList,
+    }"""
+   return render(request, 'search.html')
 
 def addData(request):
    return render(request, 'addData.html')
