c63d5f9fe7c5e175b66424cc20ce5121e15f24cd
takatoshiinaoka <heavenhayao2316@gmail.com>
2021-09-02 15:39:59 +0900

    cale表示

--- calendarSite/views.py
+++ calendarSite/views.py
@@ -43,6 +43,12 @@ def addData(request):
    user='かいた'
    subject='おーいお茶'
    title='伊藤園'
+   caleList=[{
+      'id':'',
+      'title':'ワクチン',
+      'date':'2021-09-03',
+      'user':'',
+   }]
 
    form=addDataForm(request.POST or None)
 
@@ -51,14 +57,22 @@ def addData(request):
    subject=form['subject'].data or ''
    title=form['title'].data or ''
 
+   caleList[0]['date']=date  
+   caleList[0]['title']=title  
    #print(form)
 
    print(str(date))
    print(str(user))
    print(str(subject))
    print(str(title))
+   print(caleList)
+
    if date!=''and user!='' and subject!='' and title!='':
-      return render(request,'user.html')
+      return render(request,'user.html',{
+         "caleList":caleList,
+         "date":date,
+         "title":title
+      })
    return render(request, 'addData.html')
 
 def user(request):
