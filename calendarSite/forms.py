bae5d099a70ddf0fce23f528ebed3b6b759ff6ed
takatoshiinaoka <heavenhayao2316@gmail.com>
2021-09-02 10:09:20 +0900

    稲岡

--- calendarSite/forms.py
+++ calendarSite/forms.py
@@ -5,39 +5,42 @@ from django.core.files.storage import default_storage
 from django import forms
 
 class addDataForm(forms.Form):
-    subject={}
-    title={}
-    data={}
-    user={}
+    subject= forms.CharField(
+        max_length=200,
+        required=True
+    )
+    title=()
+    data=()
+    user=()
 
 
 class indexForm(forms.Form):
-    subject={}
-    title={}
-    data={}
-    user={}
+    subject=()
+    title=()
+    data=()
+    user=()
 
 
 
 class searchForm(forms.Form):
-    subject={}
-    title={}
-    data={}
-    user={}
+    subject=()
+    title=()
+    data=()
+    user=()
 
     
 
 class subjectForm(forms.Form):
-    subject={}
-    title={}
-    data={}
-    user={}
+    subject=()
+    title=()
+    data=()
+    user=()
 
 
 
 class userForm(forms.Form):
-    subject={}
-    title={}
-    data={}
-    user={}
+    subject=()
+    title=()
+    data=()
+    user=()
 
