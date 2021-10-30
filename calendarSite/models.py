709778036e27c8fd47a503f68bae2800aecd72b5
Yuki326 <s20a1035@gmail.com>
2021-10-31 07:52:40 +0900

    コメントを追加

--- calendarSite/models.py
+++ calendarSite/models.py
@@ -26,11 +26,11 @@ class Friend(models.Model):
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     
-    def __str__(self):
+    def __str__(self):#表示を決める部分
         return self.name
 
 class Task(models.Model):
-    subject_id = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    subject_id = models.ForeignKey(Subject,on_delete=models.CASCADE)#科目テーブルと紐づけ
     name = models.CharField(max_length=100)
 
     
