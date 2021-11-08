1f372f5c78c76ee57a303b1315f5ebd722800776
Yuki326 <s20a1035@gmail.com>
2021-11-08 16:24:04 +0900

    レイアウトの改善

--- calendarProject/settings.py
+++ calendarProject/settings.py
@@ -127,11 +127,11 @@ STATIC_URL = '/static/'
 
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
-
+import os
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
