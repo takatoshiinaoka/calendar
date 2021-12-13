77cf9f04dce108218e13505ab2a31455732fce2c
Yuki326 <s20a1035@gmail.com>
2021-12-13 12:13:53 +0900

    commit

--- dbreset.sh
+++ dbreset.sh
@@ -3,9 +3,10 @@ cd calendarProject
 rm -d -r migrations
 cd ..
 rm -d -r db.sqlite3
-#python manage.py makemigrations calendarSite
-#python manage.py migrate
-#python manage.py createsuperuser
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
 
 
 # bash dbreset.sh で実行
