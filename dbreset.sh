74b80fe21325d418ca7589deee66986b8f097019
Yuki326 <s20a1035@gmail.com>
2021-12-14 12:21:29 +0900

    履修している科目を表示する機能の実装完了

--- dbreset.sh
+++ dbreset.sh
@@ -3,9 +3,11 @@ cd calendarProject
 rm -d -r migrations
 cd ..
 rm -d -r db.sqlite3
-#python manage.py makemigrations calendarSite
-#python manage.py migrate
-#python manage.py createsuperuser
+python manage.py makemigrations calendarSite
+python manage.py migrate
+python manage.py createsuperuser
+start http://localhost:8000
+python manage.py runserver
 
 
 # bash dbreset.sh で実行
